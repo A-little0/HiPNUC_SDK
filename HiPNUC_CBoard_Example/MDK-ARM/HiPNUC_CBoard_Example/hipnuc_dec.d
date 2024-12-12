db6668ab2bd74b24c02047d5715ac31fb62afa92
jiangnan <3304524476@qq.com>
2024-12-12 17:49:10 +0800

    移植超核历程，并使用空闲中断测试接收，success

---
+++ HiPNUC_CBoard_Example/MDK-ARM/HiPNUC_CBoard_Example/hipnuc_dec.d
@@ -0,0 +1,5 @@
+hipnuc_cboard_example\hipnuc_dec.o: ..\Bsp\hipnuc_dec.c
+hipnuc_cboard_example\hipnuc_dec.o: ..\Bsp\hipnuc_dec.h
+hipnuc_cboard_example\hipnuc_dec.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+hipnuc_cboard_example\hipnuc_dec.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+hipnuc_cboard_example\hipnuc_dec.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
---
+++ HiPNUC_CBoard_Example/Test/hipnuc_demo.h
@@ -0,0 +1,10 @@
+#ifndef __HIPNUC_DEMO_H
+#define __HIPNUC_DEMO_H
+
+#include "usart.h"
+#include "hipnuc_dec.h"
+
+void hipnuc_Init(void);
+
+void IDLE_IMU_Handler(void);
+#endif
